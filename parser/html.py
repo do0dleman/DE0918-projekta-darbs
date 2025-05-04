e168a3d869b4944ceab97415911e68fe260aabf5
do0dleman <nikita.obrazcovs@gmail.com>
2025-05-04 11:58:32 +0300

    added support for self closing tags, added more search functionality for the HtmlTree

--- parser/html.py
+++ parser/html.py
@@ -1,5 +1,5 @@
 from html.parser import HTMLParser
-from html.entities import name2codepoint
+from parser.self_closing_tags import SELF_CLOSING_TAGS
 import json
 
 class Element:
@@ -20,19 +20,21 @@ class Element:
         text_node = cls("text", [("value", string)])
         return text_node
     
-    def to_dict(self):
-        dict_children = list(map(lambda child: child.to_dict(), self.children))
-        return {
+    def to_dict(self, include_chldren=False):
+        dict_element = {       
             "tag": self.tag,
             "attrs": self.attrs,
-            "children": dict_children
         }
+        if include_chldren:
+            dict_element["children"] = list(map(lambda child: child.to_dict(include_chldren), self.children))
+
+        return dict_element
     
     def get_id(self):
-        return self.attrs.get("id")
+        return self.attrs.get("id") or ""
     
-    def print(self):
-        print(json.dumps(self.to_dict(),indent=2))
+    def print(self, include_chldren=False):
+        print(json.dumps(self.to_dict(include_chldren),indent=2))
             
         
 
@@ -44,10 +46,17 @@ class HtmlTree(HTMLParser):
         self.root = Element("document")
         self.current = self.root
 
+    @classmethod
+    def fromElement(cls, element):
+        new_tree = cls()
+        new_tree.root = element
+        new_tree.current = element
+
     def handle_starttag(self, tag, attrs):
         new_element = Element(tag, attrs, self.current)
         self.current.children.append(new_element)
-        self.current = new_element
+        if tag not in SELF_CLOSING_TAGS:
+            self.current = new_element
 
     def handle_endtag(self, tag):
         if self.current.parent:
@@ -56,11 +65,11 @@ class HtmlTree(HTMLParser):
     def handle_data(self, data):
         self.current.children.append(Element.fromString(data))
 
-    def to_dict(self):
-        return self.root.to_dict()
+    def to_dict(self, include_chldren=True):
+        return self.root.to_dict(include_chldren)
     
-    def print(self):
-        print(json.dumps(self.to_dict(),indent=2))
+    def print(self, include_chldren=True):
+        print(json.dumps(self.to_dict(include_chldren),indent=2))
     
     def get_by_id(self, id, element=None):
         if element==None:
@@ -74,3 +83,19 @@ class HtmlTree(HTMLParser):
             if foundElement is not None:
                 return foundElement
             
+    def get_by_tag(self, tag, element=None):
+        foundElements = []
+        
+        if element==None:
+          element = self.root
+        
+
+        if element.tag == tag:
+            foundElements.append(element)
+
+        for child in element.children:
+            moreFoundElement = self.get_by_tag(tag, element=child)
+            for element in moreFoundElement:
+                foundElements.append(element)
+
+        return foundElements
---
+++ parser/self_closing_tags.py
@@ -0,0 +1,15 @@
+SELF_CLOSING_TAGS = {
+    "area",
+    "base",
+    "br",
+    "col",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "link",
+    "meta",
+    "source",
+    "track",
+    "wbr"
+}
